a01dd49174ab83819045ad19425b7f2d3fa9d635
KJ <kj.schmidt913@gmail.com>
2020-02-07 15:02:27 -0600

    added form

--- frontend/src/components/requestForm.jsx
+++ frontend/src/components/requestForm.jsx
@@ -102,65 +102,34 @@ class RequestForm extends Component {
     render() {
         const { data } = this.state;
         return (
-            // <div>
-            //   <ul>
-            //     {data.length <= 0
-            //       ? 'NO DB ENTRIES YET'
-            //       : data.map((dat) => (
-            //           <li style={{ padding: '10px' }} key={data.message}>
-            //             <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
-            //             <span style={{ color: 'gray' }}> data: </span>
-            //             {dat.message}
-            //           </li>
-            //         ))}
-            //   </ul>
-            //   <div style={{ padding: '10px' }}>
-            //     <input
-            //       type="text"
-            //       onChange={(e) => this.setState({ message: e.target.value })}
-            //       placeholder="add something in the database"
-            //       style={{ width: '200px' }}
-            //     />
-            //     <button onClick={() => this.putDataToDB(this.state.message)}>
-            //       ADD
-            //     </button>
-            //   </div>
-            //   <div style={{ padding: '10px' }}>
-            //     <input
-            //       type="text"
-            //       style={{ width: '200px' }}
-            //       onChange={(e) => this.setState({ idToDelete: e.target.value })}
-            //       placeholder="put id of item to delete here"
-            //     />
-            //     <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-            //       DELETE
-            //     </button>
-            //   </div>
-            //   <div style={{ padding: '10px' }}>
-            //     <input
-            //       type="text"
-            //       style={{ width: '200px' }}
-            //       onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-            //       placeholder="id of item to update here"
-            //     />
-            //     <input
-            //       type="text"
-            //       style={{ width: '200px' }}
-            //       onChange={(e) => this.setState({ updateToRequestFormly: e.target.value })}
-            //       placeholder="put new value of the item here"
-            //     />
-            //     <button
-            //       onClick={() =>
-            //         this.updateDB(this.state.idToUpdate, this.state.updateToRequestFormly)
-            //       }
-            //     >
-            //       UPDATE
-            //     </button>
-            //   </div>
-            // </div>
+            <div class="container">
+                <form>
+                    <div class="col-6">
+                        <h1>Request your data</h1>
+                        <div class="form-group">
+                            <label for="firstName">First name:</label>
+                            <input type="text" id="firstName" class="form-control" />
 
-            <div >
-                <p> hi </p>
+                        </div>
+                        <div class="form-group">
+                            <label for="lasttName">Last name:</label>
+                            <input type="text" id="lastName" class="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" class="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label for="residence">Place of Residence:</label>
+                            <select class="form-control" id="residence">
+                                <option>California</option>
+                                <option>Other</option>
+                            </select>
+                        </div>
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+
             </div>
         );
     }
