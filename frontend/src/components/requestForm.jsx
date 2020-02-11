fa8a60c5e126a9b2dc1595988f6d0312bf7c09b8
KJ <kj.schmidt913@gmail.com>
2020-02-10 20:21:41 -0600

    updated request form file with much clearer functions for updating state. need to fill in database information

--- frontend/src/components/requestForm.jsx
+++ frontend/src/components/requestForm.jsx
@@ -2,82 +2,54 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class RequestForm extends Component {
-    // initialize our state
-    state = {
-        data: [],
-        id: 0,
-        message: null,
-        intervalIsSet: false,
-        idToDelete: null,
-        idToUpdate: null,
-        objectToUpdate: null,
-    };
 
-    // when component mounts, first thing it does is fetch all existing data in our db
-    // then we incorporate a polling logic so that we can easily see if our db has
-    // changed and implement those changes into our UI
-    componentDidMount() {
-        this.getDataFromDb();
-        if (!this.state.intervalIsSet) {
-            let interval = setInterval(this.getDataFromDb, 1000);
-            this.setState({ intervalIsSet: interval });
+    constructor(props) {
+        super(props)
+
+        // Setting up functions
+        this.onFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeEmail = this.onChangEmail.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        // Setting up state
+        this.state = {
+            firstName: '',
+            email: '',
+            lastName: ''
         }
     }
 
-    // never let a process live forever
-    // always kill a process everytime we are done using it
-    componentWillUnmount() {
-        if (this.state.intervalIsSet) {
-            clearInterval(this.state.intervalIsSet);
-            this.setState({ intervalIsSet: null });
-        }
+    onChangeFirstName(e) {
+        this.setState({ firstName: e.target.value })
     }
 
-    // just a note, here, in the front end, we use the id key of our data object
-    // in order to identify which we want to Update or delete.
-    // for our back end, we use the object id assigned by MongoDB to modify
-    // data base entries
+    onChangeEmail(e) {
+        this.setState({ email: e.target.value })
+    }
 
-    // our first get method that uses our backend api to
-    // fetch data from our data base
-    getDataFromDb = () => {
-        fetch('http://localhost:3001/api/getData')
-            .then((data) => data.json())
-            .then((res) => this.setState({ data: res.data }));
-    };
+    onChangeLastName(e) {
+        this.setState({ lastName: e.target.value })
+    }
 
-    // our put method that uses our backend api
-    // to create new query into our data base
-    putDataToDB = (message) => {
-        let currentIds = this.state.data.map((data) => data.id);
-        let idToBeAdded = 0;
-        while (currentIds.includes(idToBeAdded)) {
-            ++idToBeAdded;
-        }
+    onSubmit(e) {
+        e.preventDefault()
 
-        axios.post('http://localhost:3001/api/putData', {
-            id: idToBeAdded,
-            message: message,
+        const requestObject = {
+            firstName: this.state.firstName,
+            email: this.state.email,
+            lastName: this.state.lastName
+        };
+
+        axios.post('OUR LOCAL DB HERE', requestObject)
+            .then(res => console.log(res.data));
+
+        this.setState({
+            firstName: '',
+            email: '',
+            lastName: ''
         });
-    };
-
-
-    // our update method that uses our backend api
-    // to overwrite existing data base information
-    updateDB = (idToUpdate, updateToRequestFormly) => {
-        let objIdToUpdate = null;
-        parseInt(idToUpdate);
-        this.state.data.forEach((dat) => {
-            if (dat.id == idToUpdate) {
-                objIdToUpdate = dat._id;
-            }
-        });
-
-        axios.post('http://localhost:3001/api/updateData', {
-            id: objIdToUpdate,
-            update: { message: updateToRequestFormly },
-        });
-    };
+    }
 
     render() {
         const { data } = this.state;
@@ -88,25 +60,26 @@ class RequestForm extends Component {
                         <h1>Request your data</h1>
                         <div class="form-group">
                             <label for="firstName">First name:</label>
-                            <input type="text" id="firstName" class="form-control" onChange={(e) => this.setState({ message: e.target.value })}/>
+                            <input type="text" id="firstName" class="form-control" value={this.state.firstName} onChange={this.onChangeFirstName} />
 
                         </div>
                         <div class="form-group">
                             <label for="lasttName">Last name:</label>
-                            <input type="text" id="lastName" class="form-control" onChange={(e) => this.setState({ message: e.target.value })}/>
+                            <input type="text" id="lastName" class="form-control" value={this.state.lastName} onChange={this.onChangeLastName} />
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" id="email" class="form-control" onChange={(e) => this.setState({ message: e.target.value })}/>
+                            <input type="email" id="email" class="form-control" value={this.state.email} onChange={this.onChangeEmail}/>
                         </div>
-                        <div class="form-group">
+                        {/* need to go over drop down menu selection */}
+                        {/* <div class="form-group">
                             <label for="residence">Place of Residence:</label>
                             <select class="form-control" id="residence" onChange={(e) => this.setState({ message: e.target.value })}>
                                 <option>California</option>
                                 <option>Other</option>
                             </select>
-                        </div>
-                        <button class="btn btn-primary" onClick={() => this.putDataToDB(this.state.message)}>Submit</button>
+                        </div> */}
+                        <button class="btn btn-primary" type="submit">Submit</button>
                     </div>
                 </form>
 
