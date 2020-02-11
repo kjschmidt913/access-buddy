7431186343acc6bd2693917900c9b4dc8c2a3805
KJ <kj.schmidt913@gmail.com>
2020-02-11 10:34:42 -0600

    added a homepage. its an example of a functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import RequestForm from "./components/requestForm.jsx";
+import Home from "./components/home.jsx";
 
 
 function App() {
     return (<Router>
 
         <Switch>
-            {/* <Route exact path='/' component={Home} /> */}
+            <Route exact path='/' component={Home} />
             <Route path="/request-form" component={RequestForm} />
 
         </Switch>
