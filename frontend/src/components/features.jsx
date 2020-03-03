3f65e2d242b79c95c4e6c159d2f43115bc8e3a77
Will <williamdong97@gmail.com>
2020-03-03 16:53:08 -0600

    Create features.jsx

---
+++ frontend/src/components/features.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './nav.jsx';
+
+function Features() {
+    return (
+        <div>
+            <Nav account="False" />
+            <div className="container">
+                Features go here
+            </div>
+        </div>
+    );
+}
+
+export default Features;
