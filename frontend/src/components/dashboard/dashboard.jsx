8434b18e3637fc28e0545579de2a2740293423b6
Will <williamdong97@gmail.com>
2020-03-03 16:02:22 -0600

    Update dashboard.jsx

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -42,8 +42,6 @@ class Dashboard extends Component {
     };
 
 
-
-
     render() {
         if (this.state.data.length === 0) {
             return (
@@ -54,8 +52,9 @@ class Dashboard extends Component {
                 <div>
                     <Nav account = "True"/>
                     <div className="container">
+                    <h2>Active Requests</h2>
                         {/* <h2>Most recent submission:</h2>
-                        
+
                         <p><span className="font-weight-bold">First Name: </span>{this.state.data[this.state.data.length-1].firstName}</p>
                         <p><span className="font-weight-bold">Last Name: </span>{this.state.data[this.state.data.length-1].lastName}</p>
                         <p><span className="font-weight-bold">Email: </span>{this.state.data[this.state.data.length-1].email}</p>
