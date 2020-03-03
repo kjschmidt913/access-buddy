9298a99720f00ed0e6978a03f03331f1dae643e8
Will <williamdong97@gmail.com>
2020-03-03 16:53:12 -0600

    Create pricing.jsx

---
+++ frontend/src/components/pricing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './nav.jsx';
+
+function Pricing() {
+    return (
+        <div>
+            <Nav account="False" />
+            <div className="container">
+                Pricing goes here
+            </div>
+        </div>
+    );
+}
+
+export default Pricing;
