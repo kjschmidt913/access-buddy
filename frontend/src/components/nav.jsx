bca61cd8356f63076ff320ef8c1f4fce0ed48638
Will <williamdong97@gmail.com>
2020-03-03 16:53:10 -0600

    Update nav.jsx

--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -43,13 +43,13 @@ function Nav(props) {
             <div className="collapse navbar-collapse ml-3" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <span className="nav-link" >Features</span>
+                        <Link className="nav-link" to ="/features" >Features</Link>
                     </li>
                     <li className="nav-item">
-                        <span className="nav-link">Pricing</span>
+                        <Link className="nav-link" to ="/pricing">Pricing</Link>
                     </li>
                     <li className="nav-item">
-                        <span className="nav-link">About</span>
+                        <Link className="nav-link" to="/about">About</Link>
                     </li>
                 </ul>
             </div>
